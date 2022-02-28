bbe71ae5a36fa3d12d6006fe020a31175e442c53
Gundu Rithindatta <67307949+rithindattag@users.noreply.github.com>
2022-02-28 19:20:34 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
-#include<array>
+#include<array>   // array STL header file
+
 using namespace std;
 
 int main(){
-    array<int,4> a={1,2,3,4};
+    array<int,4> a={1,2,3,4};   // declaration of array STL 
     
-    int size=a.size();
+    int size=a.size();   // returns size of the array
+    
     cout << "Size of array is: " << size << endl;
     
-    cout << "Element at 2nd index is: " << a.at(2) << endl;
+    cout << "Element at 2nd index is: " << a.at(2) << endl;   // at() returns the element at that index
     
-    cout << "First element is: " << a.front() << endl;
+    cout << "First element is: " << a.front() << endl;   // front() returns the first element of the array
     
-    cout << "Last element is: " << a.back() << endl;
+    cout << "Last element is: " << a.back() << endl;   // back() returns the last element of the array
     
     
 }
